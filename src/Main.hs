ef69d6201ebd45d962e2f173ef9c797ff2761f5a
carl <carl.hedgren@gmail.com>
2017-07-31 23:55:51 +0200

    wip add a parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,39 @@
 {-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Main where
-import           Data.Map
 import           Data.Semigroup     ()
 import           Data.Text          (pack)
 import           Options.Generic
-import           Prelude            (Either (..), IO, Maybe (..), Show (..),
-                                     putStrLn, show, ($))
+import           Prelude            (Either (..), IO, Int, Maybe (..),
+                                     Show (..), String, putStrLn, show, ($))
 import           System.Environment (lookupEnv)
-import           Web.JWT            (Algorithm (HS256), ClaimsMap,
-                                     NumericDate (..), StringOrURI (..), aud,
-                                     def, encodeSigned, exp, iat, iss, nbf,
-                                     numericDate, secret, sub)
+import           Web.JWT            (Algorithm (HS256), ClaimsMap, NumericDate,
+                                     StringOrURI, aud, def, encodeSigned, exp,
+                                     iat, iss, nbf, numericDate, secret, sub)
 
-data Args = Args { issuer     :: Maybe StringOrURI
-                 , subject    :: Maybe StringOrURI
-                 , notBefore  :: Maybe NumericDate
-                 , expiration :: Maybe NumericDate
-                 , issuedAt   :: Maybe NumericDate
+data Args = Args { issuer     :: Maybe String <?> "Optional, the issuer"
+                 , subject    :: Maybe String <?> "Optional, the subject"
+                 , notBefore  :: Maybe Int <?> "Optional valid from"
+                 , expiration :: Maybe Int <?> "Optional expiration date"
+                 , issuedAt   :: Maybe Int <?> "Optional, issued at"
            --      , miscClaims :: ClaimsMap
-                 } deriving (Show, Generic)
-
-
+                 } deriving (Show, Generic, ParseRecord)
 
 main :: IO ()
 main = do
-  claims <- getRecord "Jwt Token Maker"
+  args <- getRecord "jwt generator"
   key <- lookupEnv "key"
-  case key of
+  makeJwt key (args :: Args)
+
+
+
+makeJwt :: Maybe String -> Args -> IO ()
+makeJwt key _ =   case key of
     Just s -> let claimSet = def { exp = numericDate 3
                                  }
       in putStrLn $ show $ encodeSigned HS256 (secret $ pack s) claimSet
