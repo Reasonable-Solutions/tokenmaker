f0064580971e0388063a39048ca92f1de1483d0b
carl <carl.hedgren@gmail.com>
2017-07-31 23:55:51 +0200

    wip add a parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,37 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
+import           Data.Map
+import           Data.Semigroup     ()
 import           Data.Text          (pack)
-import           Prelude            (IO, Maybe (..), putStrLn, show, ($))
+import           Options.Generic
+import           Prelude            (Either (..), IO, Maybe (..), Show (..),
+                                     putStrLn, show, ($))
 import           System.Environment (lookupEnv)
-import           Web.JWT            (Algorithm (HS256), def, encodeSigned, iat,
-                                     numericDate, secret)
+import           Web.JWT            (Algorithm (HS256), ClaimsMap,
+                                     NumericDate (..), StringOrURI (..), aud,
+                                     def, encodeSigned, exp, iat, iss, nbf,
+                                     numericDate, secret, sub)
+
+data Args = Args { issuer     :: Maybe StringOrURI
+                 , subject    :: Maybe StringOrURI
+                 , notBefore  :: Maybe NumericDate
+                 , expiration :: Maybe NumericDate
+                 , issuedAt   :: Maybe NumericDate
+           --      , miscClaims :: ClaimsMap
+                 } deriving (Show, Generic)
+
+
 
 main :: IO ()
 main = do
+  claims <- getRecord "Jwt Token Maker"
   key <- lookupEnv "key"
   case key of
-    Just s -> let claimSet = def { iat = numericDate 12312312 }
+    Just s -> let claimSet = def { exp = numericDate 3
+                                 }
       in putStrLn $ show $ encodeSigned HS256 (secret $ pack s) claimSet
     Nothing -> putStrLn "Provide key"
