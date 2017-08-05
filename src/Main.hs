cb16b92564adaec93a95387ffe949d4788ab6b6f
carl <carl.hedgren@gmail.com>
2017-08-06 01:27:04 +0200

    add verify and create

--- src/Main.hs
+++ src/Main.hs
@@ -6,35 +6,52 @@
 {-# LANGUAGE TypeOperators       #-}
 
 module Main where
+import           Control.Monad      (join)
 import           Data.Semigroup     ()
 import           Data.Text          (pack)
 import           Options.Generic
 import           Prelude            (Either (..), IO, Int, Maybe (..),
-                                     Show (..), String, putStrLn, show, ($))
+                                     Show (..), String, putStrLn, show,
+                                     undefined, ($), (.), (<$>), (<*>), (>>=))
 import           System.Environment (lookupEnv)
-import           Web.JWT            (Algorithm (HS256), ClaimsMap, NumericDate,
-                                     StringOrURI, aud, def, encodeSigned, exp,
-                                     iat, iss, nbf, numericDate, secret, sub)
+import           Web.JWT            (Algorithm (HS256), ClaimsMap, JSON,
+                                     NumericDate, Signature, StringOrURI,
+                                     VerifiedJWT, aud, decodeAndVerifySignature,
+                                     def, encodeSigned, exp, iat, iss, nbf,
+                                     numericDate, secret, signature,
+                                     stringOrURI, sub)
 
-data Args = Args { issuer     :: Maybe String <?> "Optional, the issuer"
-                 , subject    :: Maybe String <?> "Optional, the subject"
-                 , notBefore  :: Maybe Int <?> "Optional valid from"
-                 , expiration :: Maybe Int <?> "Optional expiration date"
-                 , issuedAt   :: Maybe Int <?> "Optional, issued at"
+data Args =
+  Create { issuer     :: Maybe String
+         , subject    :: Maybe String
+         , notBefore  :: Maybe Int
+         , expiration :: Maybe Int
+         , issuedAt   :: Maybe Int
            --      , miscClaims :: ClaimsMap
-                 } deriving (Show, Generic, ParseRecord)
+         }
+  | Validate { token :: String } deriving (Show, Generic, ParseRecord)
 
 main :: IO ()
-main = do
-  args <- getRecord "jwt generator"
-  key <- lookupEnv "key"
-  makeJwt key (args :: Args)
+main = getRecord "jwt gen" >>=
+   (\args -> lookupEnv "key" >>=
+     \key -> makeJwt key (args :: Args))
 
+validate :: String -> Args -> Maybe Signature
+validate key (Validate t) =
+  decodeAndVerifySignature (secret $ pack key) (pack t) >>= signature
 
+create :: String -> Args -> JSON
+create s (Create is su nb ex at) =
+  encodeSigned HS256 (secret $ pack s) claims
+  where claims = def { iss = join $ stringOrURI . pack <$> is
+                     , sub = join $ stringOrURI . pack <$> su
+                     }
 
 makeJwt :: Maybe String -> Args -> IO ()
-makeJwt key _ =   case key of
-    Just s -> let claimSet = def { exp = numericDate 3
-                                 }
-      in putStrLn $ show $ encodeSigned HS256 (secret $ pack s) claimSet
+makeJwt key (Validate t) = case key of
+    Just s  -> putStrLn $ show $ validate s (Validate t)
     Nothing -> putStrLn "Provide key"
+
+makeJwt key (Create is su no ex isu) = case key of
+  Just s  -> putStrLn $ show $ create s (Create is su no ex isu)
+  Nothing -> putStrLn "Provide key"
