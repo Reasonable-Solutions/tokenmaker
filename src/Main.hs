18e1a20f744e77ac2e2c13c95fa52a78f2606be8
carl <carl.hedgren@gmail.com>
2017-08-14 20:00:29 +0200

    add possibility to create jwts with diff expiry dates

--- src/Main.hs
+++ src/Main.hs
@@ -10,17 +10,16 @@ module Main where
 import           Control.Monad      (join)
 import           Data.Semigroup     ()
 import           Data.Text          (pack)
+import           Data.Time          (NominalDiffTime)
 import           Options.Generic
-import           Prelude            (IO, Integer, Maybe (..),
-                                     Show (..), String, fromInteger, putStrLn,
-                                     show, undefined, ($), (.), (<$>), (<*>),
-                                     (>>=))
+import           Prelude            (IO, Integer, Maybe (..), Show, String,
+                                     fromInteger, putStrLn, show, undefined,
+                                     ($), (.), (<$>), (<*>), (>>=), (=<<))
 import           System.Environment (lookupEnv)
-import           Web.JWT            (Algorithm (HS256), JSON,
-                                     Signature, StringOrURI,
-                                     VerifiedJWT, aud, decodeAndVerifySignature,
-                                     def, encodeSigned, exp, iat, intDate, iss,
-                                     nbf, numericDate, secret, signature,
+import           Web.JWT            (Algorithm (HS256), JSON, Signature, aud,
+                                     decodeAndVerifySignature, def,
+                                     encodeSigned, exp, iat, intDate, iss, nbf,
+                                     numericDate, secret, signature,
                                      stringOrURI, sub)
 
 data Args
@@ -29,28 +28,31 @@ data Args
            , notBefore  :: Maybe Integer <?> "commencement date"
            , expiration :: Maybe Integer <?> "Expiration date"
            , issuedAt   :: Maybe Integer <?> "issue date"
-           --      , miscClaims :: ClaimsMap
-            }
+           }
   | Validate String
   deriving (Generic, Show, ParseRecord)
 
 main :: IO ()
 main =
   getRecord "jwt gen" >>=
-  (\args -> lookupEnv "key" >>= \key -> makeJwt key (args :: Args))
+  (\args -> lookupEnv "key" >>=
+   \key -> makeJwt key (args :: Args))
 
 validate :: String -> Args -> Maybe Signature
 validate key (Validate t) =
   decodeAndVerifySignature (secret $ pack key) (pack t) >>= signature
-validate _ (Validate _) = undefined
+validate _ (Create _ _ _ _ _) = undefined
 
 create :: String -> Args -> JSON
 create s (Create is su nb ex at) = encodeSigned HS256 (secret $ pack s) claims
   where
     claims =
       def
-      { iss = join $ stringOrURI . pack <$> unHelpful is
-      , sub = join $ stringOrURI . pack <$> unHelpful su
+      { iss = stringOrURI =<< pack <$> unHelpful is
+      , sub = stringOrURI =<< pack <$> unHelpful su
+      , nbf = numericDate =<< toNomDT <$> unHelpful nb
+      , exp = numericDate =<< toNomDT <$> unHelpful ex
+      , iat = numericDate =<< toNomDT <$> unHelpful at
       }
 create _ (Validate _) = undefined
 
@@ -63,3 +65,6 @@ makeJwt key (Create is su no ex isu) =
   case key of
     Just s  -> putStrLn $ show $ create s (Create is su no ex isu)
     Nothing -> putStrLn "Provide key"
+
+toNomDT :: Integer -> NominalDiffTime
+toNomDT = fromInteger
